6a1c960f01cf054dad8965d005d106b98cd54411
sackidude <nils.nilsen9@outlook.com>
2024-03-29 15:40:14 +0100

    init poop zigga

---
+++ notes.md
@@ -0,0 +1,24 @@
+# Matcher här?
+
+https://www.unibet.com/sportsbook-feeds/breadcrumbs/event/1020336490?ncid=1711648163
+
+# Odds här?
+
+https://eu-offering-api.kambicdn.com/offering/v2018/ub/betoffer/event/1020336490.json?lang=en_GB&market=ZZ&ncid=1711648164
+
+https://eu-offering-api.kambicdn.com/offering/v2018/ubse/betoffer/event/1020336490.json?lang=sv_SE&market=SE&client_id=2&channel_id=1&ncid=1711718705705&includeParticipants=true
+
+## betOffers
+
+array med olika "offers"
+
+offer:
+
+- criterion = information om match
+- outcomes = olika utfall och deras odds
+
+## events
+
+## prePacks
+
+Verkar innehålla olika kombinationer.
---
+++ src/main.rs
@@ -0,0 +1,183 @@
+#![allow(dead_code)]
+
+const URL: &'static str = "https://eu-offering-api.kambicdn.com/offering/v2018/ubse/betoffer/event/1020336490.json?lang=sv_SE&market=SE&client_id=2&channel_id=1&ncid=1711718705705&includeParticipants=true";
+
+#[derive(serde::Deserialize)]
+struct Criterion {
+    id: u32,
+    label: String,
+    #[serde(rename = "englishLabel")]
+    english_label: String,
+    order: Vec<String>,
+    #[serde(rename = "occurenceType")]
+    occurence_type: Option<String>,
+    lifetime: Option<String>,
+}
+
+#[derive(serde::Deserialize)]
+struct BetOfferType {
+    id: u32,
+    name: String,
+    #[serde(rename = "englishName")]
+    english_name: String,
+}
+
+#[derive(serde::Deserialize)]
+struct Outcome {
+    id: u32,
+    label: String,
+    #[serde(rename = "englishLabel")]
+    english_label: String,
+    odds: Option<u32>,
+    participant: Option<String>,
+    #[serde(rename = "type")]
+    bet_type: String,
+    #[serde(rename = "betOfferId")]
+    bet_offer_id: u32,
+    #[serde(rename = "changedDate")]
+    changed_date: String,
+    #[serde(rename = "participantId")]
+    participant_id: Option<u32>,
+    #[serde(rename = "oddsFractional")]
+    odds_fractional: Option<String>,
+    #[serde(rename = "oddsAmerican")]
+    odds_american: Option<String>,
+    status: String,
+    #[serde(rename = "cashOutStatus")]
+    cash_out_status: String,
+}
+
+#[derive(serde::Deserialize)]
+struct BetOffer {
+    id: u32,
+    closed: String,
+    criterion: Criterion,
+    #[serde(rename = "betOfferType")]
+    bet_offer_type: BetOfferType,
+    #[serde(rename = "eventId")]
+    event_id: u32,
+    outcomes: Vec<Outcome>,
+    tags: Vec<String>,
+}
+
+#[derive(serde::Deserialize)]
+struct Path {
+    id: u32,
+    name: String,
+    #[serde(rename = "englishLabel")]
+    english_label: String,
+    #[serde(rename = "termKey")]
+    term_key: String,
+}
+
+#[derive(serde::Deserialize)]
+struct Participant {
+    #[serde(rename = "participantId")]
+    participant_id: u32,
+    name: String,
+    scratched: bool,
+    #[serde(rename = "nonRunner")]
+    non_runner: bool,
+    home: bool,
+    #[serde(rename = "participantType")]
+    participant_type: String,
+}
+
+#[derive(serde::Deserialize)]
+struct Event {
+    id: u32,
+    name: String,
+    #[serde(rename = "nameDelimiter")]
+    name_delimiter: String,
+    #[serde(rename = "englishName")]
+    english_name: String,
+    #[serde(rename = "homeName")]
+    home_name: String,
+    #[serde(rename = "awayName")]
+    away_name: String,
+    start: String,
+    group: String,
+    #[serde(rename = "groupId")]
+    group_id: u32,
+    path: Vec<Path>,
+    #[serde(rename = "nonLiveBoCount")]
+    non_live_bo_count: u32,
+    sport: String,
+    tags: Vec<String>,
+    state: String,
+    participants: Vec<Participant>,
+}
+
+#[derive(serde::Deserialize)]
+struct Odd {
+    decimal: u32,
+    american: String,
+    fractional: String,
+}
+
+#[derive(serde::Deserialize)]
+struct WTFOutcome {
+    id: u32,
+    #[serde(rename = "eventId")]
+    event_id: u32,
+    #[serde(rename = "betOfferId")]
+    bet_offer_id: u32,
+}
+
+#[derive(serde::Deserialize)]
+struct OperationOutcome {
+    operation: String,
+    groups: Vec<WTFOutcome>,
+}
+
+#[derive(serde::Deserialize)]
+struct OperationGroup {
+    operation: String,
+    groups: Vec<OperationOutcome>,
+}
+
+#[derive(serde::Deserialize)]
+struct OddsGroup {
+    odds: Odd,
+    groups: Vec<OperationGroup>,
+}
+
+#[derive(serde::Deserialize)]
+struct Selection {
+    #[serde(rename = "selectionId")]
+    selection_id: u32,
+    label: Vec<String>,
+    status: String,
+    combination: Vec<OddsGroup>,
+    tags: Vec<String>,
+}
+
+#[derive(serde::Deserialize)]
+struct PrePack {
+    id: u32,
+    #[serde(rename = "prePackSelection")]
+    pre_pack_selection: Vec<Selection>,
+    tags: Vec<String>,
+}
+
+#[derive(serde::Deserialize)]
+struct Response {
+    #[serde(rename = "betOffers")]
+    bet_offers: Vec<BetOffer>,
+    events: Vec<Event>,
+    #[serde(rename = "prePacks")]
+    pre_packs: Vec<PrePack>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Fetching URL...");
+    let response = reqwest::get(URL).await?;
+    println!("Parsing data...");
+    let data = response.json::<Response>().await?;
+    println!(
+        "First bet offer criterion label: {}",
+        data.bet_offers.get(0).unwrap().criterion.label
+    );
+    Ok(())
+}
